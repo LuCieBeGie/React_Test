10b45164b5cfea042ef65f9986cd60e898803674
LuCieBeGie <105434870+LuCieBeGie@users.noreply.github.com>
2023-06-08 00:30:39 +0200

    update

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,10 +1,14 @@
 import { Link, Outlet } from "react-router-dom";
 import { GrReactjs } from 'react-icons/gr'
-import { GoHome } from 'react-icons/go'
+import { GoHome, GoProject } from 'react-icons/go'
+import { BiUserPin } from 'react-icons/bi'
+import { GiSkills } from 'react-icons/gi'
+import { ImEnvelop } from 'react-icons/im'
+import './Header.css';
 
 function Header() {
     return (<>
-        <nav className="navbar navbar-inverse" >
+        <nav className="navbar navbar-inverse sticky-top">
             <div className="container-fluid" >
                 <Link className="navbar-brand">Welcome <GrReactjs /></Link>
                 <ul className="nav navbar-nav navbar-right">
@@ -13,10 +17,26 @@ function Header() {
                             <GoHome size="20px" />
                         </Link>
                     </li>
-                    <li><Link to="/React_Test/about">ABOUT</Link></li>
-                    <li><Link to="/React_Test/projects">PROJECTS</Link></li>
-                    <li><Link to="/React_Test/contacts">CONTACTS</Link></li>
-                    <li><Link to="/React_Test/skills">SKILLS</Link></li>
+                    <li>
+                        <Link to="/React_Test/about">
+                            <BiUserPin size="20px" />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/React_Test/projects">
+                            <GoProject size="20px" />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/React_Test/contacts">
+                            <ImEnvelop size="20px" />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/React_Test/skills">
+                            <GiSkills size='20px'/>
+                        </Link>
+                    </li>
                 </ul>
             </div>
         </nav>
--- my-app/src/pages/Home/Home.js
+++ my-app/src/pages/Home/Home.js
@@ -1,6 +1,17 @@
-function Home(){
+import './Home.css'
+
+function Home() {
     return (<>
-    <h1> home </h1>
+        <section className="container">
+            <div className="triangle">
+                <div>
+                    <h1>Hi, I'm Lusine,
+                        <br />Junior Web Developer
+                    </h1>
+                    <button className='download'>Download CV</button>
+                </div>
+            </div>
+        </section>
     </>)
 }
 
